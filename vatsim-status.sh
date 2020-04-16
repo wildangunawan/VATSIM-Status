9b26e6bd0e55c48cf7f687382e556b0a22f8d53d
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2020-04-16 22:16:03 +0800

    Update vatsim-status.sh

--- vatsim-status.sh
+++ vatsim-status.sh
@@ -1,3 +1,10 @@
-python3 get-data.py
-python3 app.py
-python3 logging.py
+#!/bin/sh
+
+# At line 8, change your current working directory
+# to parser's directory. Otherwise, it won't work correctly.
+# Consult with your hosting provider about Python 3 path.
+# Then change "/opt/alt/python36/bin/python3" to correct path.
+
+cd ~/job/api
+/opt/alt/python36/bin/python3 get-data.py
+/opt/alt/python36/bin/python3 app.py
