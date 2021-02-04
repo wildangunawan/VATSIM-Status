ebb5838c622179643d93b62ee7e7781a2fb0ee76
Wildan Gunawan <gwildan50@gmail.com>
2021-02-04 21:19:05 +0800

    Update to new JSON3 and urllib3

--- get-data.py
+++ get-data.py
@@ -1,34 +1,41 @@
 # Import all required packages
-import urllib.request, subprocess, random
+import urllib3, random, json
 
 # VATSIM Data Link variable
 vatsimDataLink = []
 
-req = urllib.request.Request(
-    "http://status.vatsim.net/status.txt",
-    data=None, 
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    }
+# client
+http = urllib3.PoolManager()
+
+# send request
+r = http.request(
+            "GET", 
+            "http://status.vatsim.net/status.txt",
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+            }
 )
 
+# get data
+data = r.data.decode('utf-8').split("\r\n")
+
 # Iterate over data
-for line in urllib.request.urlopen(req):
-    
-    # Decode bytes to string
-    # With that splitlines() function,
-    # the line become a list with ONE value only
-    line = line.decode("utf-8").splitlines()
-    
-    # Check if url0 exists
-    # Get the value by using line[0]
-    # instead of just line
-    if "url0=" in line[0]:
+for line in data:
+    # Check if json3 exists
+    if "json3=" in line:
         # If exists, split data and get the link
-        vatsimDataLink.append(line[0].split("=")[1].replace(".txt", ".json"))
+        vatsimDataLink.append(line.split("=")[1])
 
 # Get random link to download
 linkToDownload = random.choice(vatsimDataLink)
 
 # Download the file
-urllib.request.urlretrieve(linkToDownload, 'vatsim-data.json')
+with open('vatsim-data.json', 'w') as out:
+    # send request
+    r = http.request('GET', linkToDownload)
+
+    # read data
+    data = r.data.decode('utf-8')
+
+    # save data
+    out.write(data)
