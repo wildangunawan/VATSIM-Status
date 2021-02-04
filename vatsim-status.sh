3215c062ae8bf4fca0bc4d58bffe9f8788491a4d
Wildan Gunawan <gwildan50@gmail.com>
2021-02-04 21:18:35 +0800

    Simplify command

--- vatsim-status.sh
+++ vatsim-status.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-
-# At line 8, change your current working directory
-# to parser's directory. Otherwise, it won't work correctly.
-# Consult with your hosting provider about Python 3 path.
-# Then change "/opt/alt/python36/bin/python3" to correct path.
-
-cd ~/job/api
-/opt/alt/python36/bin/python3 get-data.py
-/opt/alt/python36/bin/python3 app.py
+python get-data.py
+python app.py
